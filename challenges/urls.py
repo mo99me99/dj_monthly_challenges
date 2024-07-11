97c223527160267a1f49ec6435d60266f3820a6c
mo99me99 <m.m.hosseini1099@gmail.com>
2024-07-11 15:11:30 +0330

    returning html by use python view to render

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', view=views.index),
     path('<int:month>/', view=views.monthly_challenge_by_number),
     path('<str:month>/', view=views.monthly_challenge, name='month-challenge')
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -21,6 +21,19 @@ monthly_challenges = {
 # Create your views here.
 
 
+def index(request):
+    list_items = ''
+    months = list(monthly_challenges.keys())
+    list_items = ''
+    for month in months:
+        capatalized_month = month.capitalize()
+        month_path = reverse('month-challenge', args=[month])
+        list_items += f'\n <li><a href="{month_path}">{capatalized_month}</a></li>'
+
+    response_data = f'<ul> {list_items} </ul>'
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
@@ -28,13 +41,15 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound('Invalid Month!')
 
     redirect_month = months[month-1]
-    redirect_path = reverse('month-challenge', args=[redirect_month]) 
-    return HttpResponseRedirect(redirect_to=redirect_path) #'/challenges/'+redirect_month
+    redirect_path = reverse('month-challenge', args=[redirect_month])
+    # '/challenges/'+redirect_month
+    return HttpResponseRedirect(redirect_to=redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f'<h1>{challenge_text}</h1>'
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound('This month is not supported!')
+        return HttpResponseNotFound('<h1>This month is not supported!</h1>')
